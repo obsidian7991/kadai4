31c73d3eb71a1067ae8b560a8e5aeb8739e79054
katsu <katsu@cs16006>
2018-10-18 17:47:33 +0900

    h=0.00000000000001

---
+++ src/differential/Differential_main.java
@@ -0,0 +1,29 @@
+package differential;
+
+import java.util.Scanner;
+
+public class Differential_main {
+	
+	
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input value of  a> ");
+		double a = Double.parseDouble(scan.next());
+		System.out.print("Input value of h> ");
+		double h = Double.parseDouble(scan.next());
+		
+		Differential_lib dlib = new Differential_lib(a,h);
+		System.out.println("f(x)=x^2‚Ìx="+a+"‚É‚¨‚¯‚é”÷•ªŒW”‚Í"+dlib.getRx());
+		
+		
+		System.out.println("f(x)=x^2‚Ìx="+h+"‚É‚¨‚¯‚é”’l”÷•ª‚Í"+dlib.getDx());
+
+		System.out.println("‘Š‘ÎŒë·‚Í"+Math.abs((dlib.getRx() - dlib.getDx()) / (dlib.getRx() *100)));
+
+
+
+		
+		
+	}
+}
